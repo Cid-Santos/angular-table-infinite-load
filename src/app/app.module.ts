bd1ba081d36727fbd191f29aad914b0093743f63
Cid Santos <orium-5@hotmail.com>
2022-05-28 16:31:47 -0300

    Implementação do scroll infinit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './shared/modules/app-material.module';
 import { TableComponent } from './table/table.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { TableComponent } from './table/table.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     AppMaterialModule,
     HttpClientModule
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { PaisesModel } from '../shared/model/Paises.model';
+import { ScrollableModel } from '../shared/model/scrollable.model';
 import { AppSettingsService } from '../shared/services/app-settings.service';
 
 @Component({
@@ -9,14 +10,49 @@ import { AppSettingsService } from '../shared/services/app-settings.service';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
-  title = 'Table';
-  paises$: Observable<PaisesModel[]>;
+export class TableComponent implements OnInit, OnDestroy {
+  title = 'Material Table Infinite Scroll';
+  subtitle = 'Infinite Scroll automatically adds the next page, saving users from a full page load.';
+  selectedRowIndex: number;
 
-  constructor(private appSettingsService: AppSettingsService) { }
+  displayedColumns: string[] = ['code', 'iso3', 'name', 'formalname'];
+  dataSource$: PaisesModel[] = [];
+  closeDataSource: Subscription;
+
+  scrollable: ScrollableModel;
+
+  constructor(private appSettingsService: AppSettingsService) {
+    this.scrollable = new ScrollableModel(15, 0);
+  }
 
   ngOnInit() {
-    this.paises$ = this.appSettingsService.getPaises();
+    this.getTableData(this.scrollable);
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeDataSource) this.closeDataSource.unsubscribe();
+  }
+
+  onTableScroll(e: any) {
+    if (this.scrollable.setScrollLimit(e.target, 20))
+      this.getTableData(this.scrollable);
+  }
+
+  getTableData(scrollable: ScrollableModel) {
+    this.closeDataSource = this.appSettingsService.getPaises()
+      .subscribe((data: Array<PaisesModel>) => {
+        if (data.length > 0) {
+          this.dataSource$ = this.dataSource$.concat(
+            data.filter((value, index) =>
+              index >= scrollable.start && index < scrollable.end
+            ));
+          scrollable.updateIndex();
+        }
+      });
+  }
+
+  selectedRow(row: number) {
+    console.log('selectedRow', row);
   }
 
 }
